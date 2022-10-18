97d52e66b3989393287a761c1543ce18dd7c613a
Ludaprince <danielamuzu05@gmail.com>
2022-10-18 16:33:33 -0700

    addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two numbers together
+ * @a: the first number tonbe added
+ * @b: seconf number to be added
+ *
+ * Return: return 0 if successful
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
