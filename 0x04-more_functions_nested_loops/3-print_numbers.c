6db6bb57119309a48d40a5a718c936cbb77c3b64
Ludaprince <danielamuzu05@gmail.com>
2022-10-20 16:23:04 -0700

    printing digit from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,17 @@
 /**
  * print_numbers - prints digits from 0 to 9
  *
- * Return: Always 0
+ * Return: void
  */
 void print_numbers(void)
 {
-	char x;
+	int x;
 
-	for (x = '0'; x <= '9'; x++)
+	x = 0;
+	while (x < 10)
 	{
-		_putchar(x);
+	_putchar(x + '0');
+	x++;
 	}
 	_putchar('\n');
 }
