9ac073fd1ed5c951797d5f217106fa9e83f358ae
Ludaprince <danielamuzu05@gmail.com>
2022-10-20 06:42:41 -0700

    debugging functions

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - a function printing negative or positive integer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
