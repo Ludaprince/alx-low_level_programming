0a2de6ab83be8c51daf3ea92e7b0e0146c9cb531
Ludaprince <danielamuzu05@gmail.com>
2022-10-20 19:40:40 -0700

    finction task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _isuper - checks for uppercase
  * @c: the variable
  * Return: Always 0 or 1
  */
 
-int _isuper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
