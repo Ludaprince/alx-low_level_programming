7290e936f91244d544b4dc9c7135e090a2367aeb
Ludaprince <danielamuzu05@gmail.com>
2022-10-20 19:38:06 -0700

    function task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,13 @@
 /**
  * _isuper - checks for uppercase
  * @c: the variable
- * Return: Always 0
+ * Return: Always 0 or 1
  */
 
 int _isuper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
