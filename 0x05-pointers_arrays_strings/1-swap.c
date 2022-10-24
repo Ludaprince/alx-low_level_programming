f0246d01ea4d61e6487f518043d20f63de9d8b7f
Ludaprince <danielamuzu05@gmail.com>
2022-10-24 08:13:18 -0700

    task 1 of pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of equal int
+ * @a: first integer variable
+ * @b: second integer variable
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = x;
+	*b = x;
+	*a = *b;
+}
