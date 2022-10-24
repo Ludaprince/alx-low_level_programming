7f8cfc3d8556071903cb637a27f178be0b60c55c
Ludaprince <danielamuzu05@gmail.com>
2022-10-24 15:45:45 -0700

    task 5 on pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,22 @@
 #include "main.h"
 /**
- * rev_string - reverses a string
- * @s: input string
+ * rev_string - reverses string
+ * @s: character for reference
  * Return: void
  */
 void rev_string(char *s)
 {
-	int count = 0, i, j;
-	char *str, tmp;
+	char rv = s[0];
+	int c = 0;
+	int i;
 
-	while (count >= 0)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	str = s;
-
-	for (i = 0; i < (count - 1); i++)
-	{
-		for (j = i + 1; j > 0; j--)
-		{
-			tmp = *(str + j);
-			(str + j) = (str + (j - 1));
-			*(str + (j - 1)) = tmp;
-		}
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
 }
-
-
