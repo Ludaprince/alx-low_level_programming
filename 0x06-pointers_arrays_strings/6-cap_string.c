5621e209a0dbd8f9fd146d4e1ddd726a3df14ddc
Ludaprince <danielamuzu05@gmail.com>
2022-10-27 14:35:25 -0700

    task 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words in a 
+ * cap_string - Function that capitalizes
  * string
- * @s: string
- * Return: address of s
+ * @s: string to capitalize
+ * Return: capitalized sentence
  */
 
 char *cap_string(char *s)
 {
 	int i = 0, j;
 	char a[] = " \t\n,;.!?\"(){}";
-	
+
 	while (*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
 			if (i == 0)
-					*(s + i) -= 'a' - 'A';
+			*(s + i) -= 'a' - 'A';
 			else
 			{
 				for (j = 0; j <= 12; j++)
 				{
-					if (a[j] == *(s + i - 1)
-						*(s + i) -= 'a' - 'A';
-						}
-						}
-						}
-						i++;
-						}
-						return (s);
-						}
-								
+					if (a[j] == *(s + i - 1))
+					*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
