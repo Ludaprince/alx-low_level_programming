9b80d47bd2a00a122192615f9bc2a45085e09d46
Ludaprince <danielamuzu05@gmail.com>
2022-11-07 15:35:36 -0800

    0-create_array.c

---
+++ 0x0B-malloc_free/putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints character to stdio.h
+ * @c: character to be printed
+ * Return: void
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
