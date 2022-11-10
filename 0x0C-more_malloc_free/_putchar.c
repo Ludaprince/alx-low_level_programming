d07821be1d63e48b046deadbf0002a4482bacbce
Ludaprince <danielamuzu05@gmail.com>
2022-11-10 15:26:08 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdput
+ * @c: character to prints
+ *
+ * Return: on success 1
+ * on error, -1 is returned and it is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
+		
